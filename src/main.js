0f03cb4b787593cf8f9766897b1b336d1f0aed7b
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2020-11-24 17:05:05 +0500

    Оптимизация кода после ревью

--- src/main.js
+++ src/main.js
@@ -1,34 +1,40 @@
-import {createTripInfoTemplate} from "./views/trip-info.js";
-import {createTripCostTemplate} from "./views/trip-cost.js";
-import {createTripMenuTemplate} from "./views/trip-menu.js";
-import {createTripFiltersTemplate} from "./views/trip-filters.js";
-import {createTripSortingTemplate} from "./views/trip-sorting.js";
-import {createTripEventsListTemplate} from "./views/trip-events-list.js";
-import {createTripFormEditTemplate} from "./views/trip-event-edit.js";
-import {createTripEventTemplate} from "./views/trip-event.js";
+import {createInfoCostSectionTemplate} from "./views/info-cost-section.js";
+import {createTripInfoTemplate} from "./views/info.js";
+import {createTripCostTemplate} from "./views/cost.js";
+import {createTripMenuTemplate} from "./views/menu.js";
+import {createTripFiltersTemplate} from "./views/filters.js";
+import {createTripSortingTemplate} from "./views/sorting.js";
+import {createTripEventsListTemplate} from "./views/events-list.js";
+import {createTripFormCreateTemplate} from "./views/event-create.js";
+import {createTripFormEditTemplate} from "./views/event-edit.js";
+import {createTripEventTemplate} from "./views/event.js";
 
 const EVENT_COUNT = 3;
 
-const render = (container, template, place) => {
+const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const tripMainElement = document.querySelector(`.trip-main`);
-const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
+
+render(tripMainElement, createInfoCostSectionTemplate());
+
+const tripInfoCostSectionElement = tripMainElement.querySelector(`.trip-info`);
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 const tripEventsElement = document.querySelector(`.trip-events`);
 
-render(tripInfoElement, createTripInfoTemplate(), `beforeend`);
-render(tripInfoElement, createTripCostTemplate(), `beforeend`);
-render(tripControlsElement, createTripMenuTemplate(), `beforeend`);
-render(tripControlsElement, createTripFiltersTemplate(), `beforeend`);
-render(tripEventsElement, createTripSortingTemplate(), `beforeend`);
-render(tripEventsElement, createTripEventsListTemplate(), `beforeend`);
+render(tripInfoCostSectionElement, createTripInfoTemplate());
+render(tripInfoCostSectionElement, createTripCostTemplate());
+render(tripControlsElement, createTripMenuTemplate());
+render(tripControlsElement, createTripFiltersTemplate());
+render(tripEventsElement, createTripSortingTemplate());
+render(tripEventsElement, createTripEventsListTemplate());
 
 const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
 
-render(tripEventsListElement, createTripFormEditTemplate(), `beforeend`);
+render(tripEventsListElement, createTripFormCreateTemplate());
+render(tripEventsListElement, createTripFormEditTemplate());
 for (let i = 0; i < EVENT_COUNT; i++) {
-  render(tripEventsListElement, createTripEventTemplate(), `beforeend`);
+  render(tripEventsListElement, createTripEventTemplate());
 }
 
