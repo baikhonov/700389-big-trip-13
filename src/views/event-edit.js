54653167a14563df841034fc02b0333a288b9c4d
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2021-01-28 19:32:08 +0500

    Настроил выбор даты из календаря в форме редактрования

--- src/views/event-edit.js
+++ src/views/event-edit.js
@@ -1,6 +1,10 @@
 import {createEventDestinationsTemplate, dateForForm} from "../utils/event";
 import {EVENT_TYPES} from "../const";
+import dayjs from "dayjs";
 import SmartView from "./smart";
+import flatpickr from "flatpickr";
+
+import "../../node_modules/flatpickr/dist/flatpickr.min.css";
 
 const createFormOffersTemplate = (offers) => {
   if (offers.length === 0) {
@@ -64,74 +68,75 @@ const createFormEditTemplate = (data) => {
   const typesTemplate = createEventTypesTemplate(EVENT_TYPES);
 
   return `
-<li class="trip-events__item">
-<form class="event event--edit" action="#" method="post">
-<header class="event__header">
-  <div class="event__type-wrapper">
-    <label class="event__type  event__type-btn" for="event-type-toggle-edit">
-      <span class="visually-hidden">Choose event type</span>
-      <img class="event__type-icon" width="17" height="17" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
-    </label>
-    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-edit" type="checkbox">
+    <li class="trip-events__item">
+    <form class="event event--edit" action="#" method="post">
+    <header class="event__header">
+      <div class="event__type-wrapper">
+        <label class="event__type  event__type-btn" for="event-type-toggle-edit">
+          <span class="visually-hidden">Choose event type</span>
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
+        </label>
+        <input class="event__type-toggle  visually-hidden" id="event-type-toggle-edit" type="checkbox">
 
-    <div class="event__type-list">
-      <fieldset class="event__type-group">
-        <legend class="visually-hidden">Event type</legend>
+        <div class="event__type-list">
+          <fieldset class="event__type-group">
+            <legend class="visually-hidden">Event type</legend>
 
-        ${typesTemplate}
-      </fieldset>
-    </div>
-  </div>
+            ${typesTemplate}
+          </fieldset>
+        </div>
+      </div>
 
-  <div class="event__field-group  event__field-group--destination">
-    <label class="event__label  event__type-output" for="event-destination-edit">
-      ${type}
-    </label>
-    <input class="event__input  event__input--destination" id="event-destination-edit" type="text" name="event-destination" value="${destination}" list="destination-list-edit">
-    <datalist id="destination-list-edit">
-      ${destinationDatalist}
-    </datalist>
-  </div>
+      <div class="event__field-group  event__field-group--destination">
+        <label class="event__label  event__type-output" for="event-destination-edit">
+          ${type}
+        </label>
+        <input class="event__input  event__input--destination" id="event-destination-edit" type="text" name="event-destination" value="${destination}" list="destination-list-edit">
+        <datalist id="destination-list-edit">
+          ${destinationDatalist}
+        </datalist>
+      </div>
 
-  <div class="event__field-group  event__field-group--time">
-    <label class="visually-hidden" for="event-start-time-edit">From</label>
-    <input class="event__input  event__input--time" id="event-start-time-edit" type="text" name="event-start-time" value="${dateForForm(beginDate)}">
-    &mdash;
-    <label class="visually-hidden" for="event-end-time-edit">To</label>
-    <input class="event__input  event__input--time" id="event-end-time-edit" type="text" name="event-end-time" value="${dateForForm(endDate)}">
-  </div>
+      <div class="event__field-group  event__field-group--time">
+        <label class="visually-hidden" for="event-start-time-edit">From</label>
+        <input class="event__input  event__input--time" id="event-start-time-edit" type="text" name="event-start-time" value="${dateForForm(beginDate)}">
+        &mdash;
+        <label class="visually-hidden" for="event-end-time-edit">To</label>
+        <input class="event__input  event__input--time" id="event-end-time-edit" type="text" name="event-end-time" value="${dateForForm(endDate)}">
+      </div>
 
-  <div class="event__field-group  event__field-group--price">
-    <label class="event__label" for="event-price-edit">
-      <span class="visually-hidden">Price</span>
-      &euro;
-    </label>
-    <input class="event__input  event__input--price" id="event-price-edit" type="text" name="event-price" value="${price}">
-  </div>
+      <div class="event__field-group  event__field-group--price">
+        <label class="event__label" for="event-price-edit">
+          <span class="visually-hidden">Price</span>
+          &euro;
+        </label>
+        <input class="event__input  event__input--price" id="event-price-edit" type="text" name="event-price" value="${price}">
+      </div>
 
-  <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-  <button class="event__reset-btn" type="reset">Delete</button>
-  <button class="event__rollup-btn" type="button">
-    <span class="visually-hidden">Open event</span>
-  </button>
-</header>
-<section class="event__details">
-  ${offersTemplate}
+      <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+      <button class="event__reset-btn" type="reset">Delete</button>
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+    </header>
+    <section class="event__details">
+      ${offersTemplate}
 
-  <section class="event__section  event__section--destination">
-    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-    <p class="event__destination-description">${description}</p>
-  </section>
-</section>
-</form>
-</li>
-`;
+      <section class="event__section  event__section--destination">
+        <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+        <p class="event__destination-description">${description}</p>
+      </section>
+    </section>
+    </form>
+    </li>
+  `;
 };
 
 export default class EventEdit extends SmartView {
   constructor(event) {
     super();
     this._data = EventEdit.parseEventToData(event);
+    this._datepicker = null;
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
     this._formCloseHandler = this._formCloseHandler.bind(this);
     this._typeChangeHandler = this._typeChangeHandler.bind(this);
@@ -141,11 +146,12 @@ export default class EventEdit extends SmartView {
     this._priceInputHandler = this._priceInputHandler.bind(this);
 
     this._setInnerHandlers();
+    this._setDatepicker();
   }
 
   reset(event) {
     this.updateData(
-        EventEdit.parseEventToData(event)
+      EventEdit.parseEventToData(event)
     );
   }
 
@@ -155,8 +161,44 @@ export default class EventEdit extends SmartView {
 
   restoreHandlers() {
     this._setInnerHandlers();
+    this._setDatepicker();
     this.setFormSubmitHandler(this._callback.formSubmit);
     this.setFormCloseHandler(this._callback.formClose);
+  }
+
+  _setDatepicker() {
+    if (this._datepicker) {
+      // В случае обновления компонента удаляем вспомогательные DOM-элементы,
+      // которые создает flatpickr при инициализации
+      this._datepicker.destroy();
+      this._datepicker = null;
+    }
+
+    if (this._data.beginDate) {
+      // flatpickr есть смысл инициализировать только в случае,
+      // если поле выбора даты доступно для заполнения
+      this._datepicker = flatpickr(
+        this.getElement().querySelector(`#event-start-time-edit`),
+        {
+          dateFormat: `y/m/j H:i`,
+          defaultDate: this._data.beginDate,
+          onChange: this._beginDateInputHandler // На событие flatpickr передаём наш колбэк
+        }
+      );
+    }
+
+    if (this._data.endDate) {
+      // flatpickr есть смысл инициализировать только в случае,
+      // если поле выбора даты доступно для заполнения
+      this._datepicker = flatpickr(
+        this.getElement().querySelector(`#event-end-time-edit`),
+        {
+          dateFormat: `y/m/j H:i`,
+          defaultDate: this._data.endDate,
+          onChange: this._endDateInputHandler // На событие flatpickr передаём наш колбэк
+        }
+      );
+    }
   }
 
   _setInnerHandlers() {
@@ -191,17 +233,15 @@ export default class EventEdit extends SmartView {
     }, true);
   }
 
-  _beginDateInputHandler(evt) {
-    evt.preventDefault();
+  _beginDateInputHandler([userDate]) {
     this.updateData({
-      beginDate: evt.target.value
+      beginDate: dayjs(userDate).hour(23).minute(59).second(59).toDate()
     }, true);
   }
 
-  _endDateInputHandler(evt) {
-    evt.preventDefault();
+  _endDateInputHandler([userDate]) {
     this.updateData({
-      endDate: evt.target.value
+      endDate: dayjs(userDate).hour(23).minute(59).second(59).toDate()
     }, true);
   }
 
@@ -234,9 +274,9 @@ export default class EventEdit extends SmartView {
 
   static parseEventToData(event) {
     return Object.assign(
-        {},
-        event,
-        {}
+      {},
+      event,
+      {}
     );
   }
 
